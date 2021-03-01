dfcaa6d2d7af0b45c102d4937cc9569ccf82e514
ueda1212 <78341754+ueda1212@users.noreply.github.com>
2021-03-01 17:56:49 +0900

    実装課題の訂正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,5 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
@@ -20,18 +21,14 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype=Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments
   end
 
   def edit
-    @prototype=Prototype.find(params[:id])
   end
 
   def update
-    @prototype=Prototype.find(params[:id])
-    
     if @prototype.update(prototype_params)
       redirect_to prototype_path(@prototype)
     else
@@ -40,7 +37,6 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
-    @prototype=Prototype.find(params[:id])
     @prototype.destroy
     redirect_to root_path(@prototype)
   end
@@ -51,9 +47,13 @@ class PrototypesController < ApplicationController
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
-  
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
   def contributor_confirmation
-    redirect_to root_path unless current_user == @prototype.user
+      redirect_to root_path unless current_user == @prototype.user
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @user = User.all
-    @user = user
+    @user=user
   end
   
 end
